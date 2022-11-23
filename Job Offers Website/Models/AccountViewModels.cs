307fe4273d3b7eeee95b85b467d05937daa1199b
Mohamed Afify <mohamed.ae1717@gmail.com>
2022-11-23 03:12:45 +0200

    Register & UserName

--- Job Offers Website/Models/AccountViewModels.cs
+++ Job Offers Website/Models/AccountViewModels.cs
@@ -65,18 +65,21 @@ namespace WebApplication1.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "اسم المستخدم")]
+        public string UserName { get; set; }
+        [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "البريد الإلكترونى")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "كلمة السر")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "تأكيد كلمة السر")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
