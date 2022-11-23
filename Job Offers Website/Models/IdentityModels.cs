3612b79703a6267bcddfe9e3579bd57758296121
Mohamed Afify <mohamed.ae1717@gmail.com>
2022-11-23 20:20:19 +0200

    Create&Edit&Delete&Details

--- Job Offers Website/Models/IdentityModels.cs
+++ Job Offers Website/Models/IdentityModels.cs
@@ -30,5 +30,7 @@ namespace WebApplication1.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<Job_Offers_Website.Models.Category> Categories { get; set; }
     }
 }
